ebaa044a667c2bdec4727dd1db757327b3d7e8c6
Philip Laxamana <philip.laxamana@hotmail.com>
2020-04-11 18:43:12 -0400

    sub-desc changed width, decreased stagger effect

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ const tl = gsap.timeline({defaults: {duration: 1}});
 
 tl.from(".main-copy", {y: -50, opacity: 0})
   .to("h1 span", {clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"}, "-=.7")
-  .from("ul.featured-cabins li", {opacity: 0, x: 100, stagger: .5}, "-=.7")
+  .from("ul.featured-cabins li", {opacity: 0, x: 100, stagger: .3}, "-=.7")
